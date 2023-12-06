510789c4c0a5e0a7bc37cd76ccd4f78927cf3701
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-12-06 05:16:51 +0200

    Change CORS rules

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://todos-nest.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
-
   await app.listen(8080);
 }
 bootstrap();
